9c568db81a5f6773d7824cdfcade6626181ab809
David M. Rivero <davidmriverog@gmail.com>
2017-05-02 20:22:30 -0400

    ejercicio 2 - configuracion de database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('gender',['F','M']);
+            $table->text('biography');
             $table->rememberToken();
             $table->timestamps();
         });
